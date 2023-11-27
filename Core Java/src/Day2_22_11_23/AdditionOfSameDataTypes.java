0aa65d646419de46aa605a71542b0888b3e39a8b
naveen <naveen.madur@coditas.com>
2023-11-27 09:57:33 +0530

    Core java assignment 1 and 2

---
+++ Core Java/src/Day2_22_11_23/AdditionOfSameDataTypes.java
@@ -0,0 +1,11 @@
+package Day2_22_11_23;
+
+public class AdditionOfSameDataTypes {
+    public static void main(String[] args) {
+        short a = 30000;
+        short b = 10000;
+
+        // Cant assign addition to short c as the addition will be out of range
+        int c =  a + b;
+    }
+}
